ef0a5c61887afeeb6c5ee33b4171fdd16ccda5ac
akpax <austin.paxton00@gmail.com>
2024-05-15 13:54:02 -0700

    add big O to docstring

--- Algorithms/sort/selection_sort.py
+++ Algorithms/sort/selection_sort.py
@@ -1,4 +1,10 @@
 def selection_sort(values):
+    """
+    Finds minimum value in values, adds it to sorted,
+    and then removes it from values.
+    This is repeated until all values are sorted.
+    Takes O(n^2)
+    """
     sorted = []
     for i in range(len(values)):
         min_index = index_of_min(values)
@@ -7,6 +13,9 @@ def selection_sort(values):
 
 
 def index_of_min(values):
+    """
+    Finds index of minimum value in values
+    """
     min_index = 0
     for i in range(1, len(values)):
         if values[i] < values[min_index]:
