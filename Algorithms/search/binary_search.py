2e69d5db5d2fa67bc65514305af1b07f5801c7c8
akpax <austin.paxton00@gmail.com>
2024-05-15 13:55:27 -0700

    add time complexioty to docstring

--- Algorithms/search/binary_search.py
+++ Algorithms/search/binary_search.py
@@ -1,24 +1,26 @@
-def binary_search(input: list, target)->int:
+def binary_search(input: list, target) -> int:
     """
     Returns index position of target if found. else returns None
     Note: This is an iterative solution.
+    Takes O(log n)
     """
-    input.sort() # sort list
+    input.sort()  # sort list
     first = 0
     last = len(input) - 1
     while first <= last:
-        mid_ind = int((first+last)/2)
+        mid_ind = int((first + last) / 2)
         if input[mid_ind] == target:
             return mid_ind
         if input[mid_ind] < target:
             first = mid_ind + 1
         if input[mid_ind] > target:
-            last = mid_ind - 1 
+            last = mid_ind - 1
     return None
-    
-if __name__=="__main__":
-    input = [1,2,3,4,5,6,7,8,9,10]
+
+
+if __name__ == "__main__":
+    input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     # test algo
-    for target in range(1,11):
-        assert binary_search(input, target) == target-1
+    for target in range(1, 11):
+        assert binary_search(input, target) == target - 1
     assert binary_search(input, 12) == None
--- Algorithms/search/linear__search.py
+++ Algorithms/search/linear__search.py
@@ -1,15 +1,15 @@
-def linear_search(input: list, target: int)->int:
+def linear_search(input: list, target: int) -> int:
     """
     Returns index position of target if found. else returns None
+    Takes O(n)
     """
-    for i,item in enumerate(input):
+    for i, item in enumerate(input):
         if item == target:
             return i
     return None
 
 
-if __name__=="__main__":
-    test_list = [1,2,3,4,5,6,7,8]
+if __name__ == "__main__":
+    test_list = [1, 2, 3, 4, 5, 6, 7, 8]
     print(linear_search(test_list, 4))
     print(linear_search(test_list, 10))
-    
